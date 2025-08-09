67e49dfbe9f25c7edae50e7e72022bb133fca64e
winfredo <winfrednukpe2002@gmail.com>
2025-08-09 23:44:25 +0000

    Change the fetch method to using async/await

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ let quoteDisplay = document.querySelector(".quote");
 let authorDisplay = document.querySelector(".author");
 let generateBtn = document.querySelector(".generate-btn");
 
-let randomQuote = () => {
-  fetch("https://dummyjson.com/quotes", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-        generateBtn.textContent = "Loading...";
-       let quotes = data.quotes;
-      let randomIndex = Math.floor(Math.random() * quotes.length);
-      let quote = quotes[randomIndex];
-      authorDisplay.textContent = quote.author;
-      quoteDisplay.textContent = quote.quote;
-        generateBtn.textContent = "Generate New Quote";
+let randomQuote = async () => {
+  try {
+    generateBtn.textContent = "Loading...";
+
+    const res = await fetch("https://dummyjson.com/quotes", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+
+    const data = await res.json();
+    let quotes = data.quotes;
+    let randomIndex = Math.floor(Math.random() * quotes.length);
+    let quote = quotes[randomIndex];
+
+    authorDisplay.textContent = quote.author;
+    quoteDisplay.textContent = quote.quote;
+    generateBtn.textContent = "Generate New Quote";
+  } catch (error) {
+    console.error("Error fetching quotes:", error);
+    generateBtn.textContent = "Try Again";
+  }
 };
+
 
 generateBtn.addEventListener("click", randomQuote);
